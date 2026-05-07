f9ad32f5048b9f6a2986f73568b07af5473e0469
Big867 <abdonaaser2007@gmail.com>
2026-05-07 07:17:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,34 +4,26 @@ const qrcode = require('qrcode-terminal');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        handleSIGINT: false,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--no-zygote'
-        ],
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
 });
 
-// رقمك هنا (تأكد من تعديله)
+// حط رقمك هنا يا ناصر
 const myNumber = '201204950121@c.us'; 
 
 client.on('qr', (qr) => {
-    console.log('امسح الكود ده يا ناصر:');
+    console.log('امسح الكود فوراً:');
     qrcode.generate(qr, {small: true});
 });
 
 client.on('ready', () => {
-    console.log('مبروك يا بطل.. البوت شغال!');
+    console.log('البوت شغال يا ناصر.. مبروك!');
 });
 
 client.on('message', async (msg) => {
     if (msg.from !== myNumber) return;
-
     if (msg.body === 'عشوائي') {
-        msg.reply('جاري الإرسال لرقم عشوائي...');
-        // هنا ممكن تضيف دالة الرقم العشوائي اللي عملناها قبل كدة
+        msg.reply('شغال يا بطل، جاري التجهيز..');
     }
 });
 
