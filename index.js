6014c072f7bd27593910d00fcf3bc0852c0d0c88
Big867 <abdonaaser2007@gmail.com>
2026-05-07 06:31:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const qrcode = require('qrcode-terminal');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
+        executablePath: '/usr/bin/google-chrome-stable',
         handleSIGINT: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--no-zygote',
+            '--single-process'
+        ],
     }
 });
 
@@ -20,20 +27,20 @@ function generateEgyptianNumber() {
 }
 
 client.on('qr', (qr) => {
-    console.log('امسح الكود ده بموبايلك:');
+    console.log('امسح الكود ده فوراً:');
     qrcode.generate(qr, {small: true});
 });
 
 client.on('ready', () => {
-    console.log('البوت جاهز وشغال!');
+    console.log('البوت شغال يا ناصر!');
     setInterval(async () => {
         const randomNumber = generateEgyptianNumber();
-        const message = "يا هلا! رسالة تجريبية عشوائية."; 
+        const message = "رسالة تجريبية من بوت ناصر."; 
         try {
             await client.sendMessage(randomNumber, message);
             console.log(`تم الإرسال لـ: ${randomNumber}`);
         } catch (err) {
-            console.log(`فشل الإرسال لـ ${randomNumber}`);
+            console.log(`فشل لـ ${randomNumber}`);
         }
     }, 300000); 
 });
